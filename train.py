5a27b2a87772e34cbff43f30163ce2e0170f2f28
NhiHuynh <nhi@scaleflex.com>
2023-06-17 17:17:11 +0700

    add model with fine tune

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 # Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -10,32 +11,38 @@ car_indices = y_train.flatten() == 1
 x_train = x_train[car_indices]
 y_train = y_train[car_indices]
 
+# Adjust the data splitting to have the same batch size for logits and labels
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, test_size=0.2, random_state=42)
+
 # Preprocess the data
 x_train = x_train.astype("float32") / 255.0
-y_train = keras.utils.to_categorical(y_train, num_classes=2)
+x_val = x_val.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
-# Build the CNN model
+# Build the CNN model with increased network size
 model = keras.Sequential([
-    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+    layers.Conv2D(64, (3, 3), activation="relu", input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.Conv2D(128, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(256, (3, 3), activation="relu"),
     layers.Flatten(),
-    layers.Dense(64, activation="relu"),
-    layers.Dense(10, activation="softmax")  # Updated num_classes
+    layers.Dense(256, activation="relu"),
+    layers.Dense(128, activation="relu"),
+    layers.Dense(10, activation="softmax")
 ])
 
 # Compile the model
 model.compile(optimizer="adam",
-              loss="sparse_categorical_crossentropy",  # Updated loss function
+              loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
 
 # Train the model
-model.fit(x_train, y_train, batch_size=64, epochs=10, validation_split=0.2)
+model.fit(x_train, y_train, batch_size=64,
+          epochs=20, validation_data=(x_val, y_val))
 
 # Evaluate the model on the test set
-x_test = x_test.astype("float32") / 255.0
-y_test = y_test.flatten()
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print("Test loss:", test_loss)
 print("Test accuracy:", test_acc)
