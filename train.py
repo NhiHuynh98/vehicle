1bbbb9a5761a205852095fc6c3ddd24adad1f90a
NhiHuynh <nhi@scaleflex.com>
2023-06-17 16:40:50 +0700

    build new model

--- train.py
+++ train.py
@@ -1,156 +1,42 @@
-# USAGE
-# python train.py --dataset dataset --model brandNet.model --labelbin lb.pickles
-
-import matplotlib
-matplotlib.use("Agg")
-
-# import the necessary packages
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers.legacy import Adam
-from tensorflow.keras.utils import img_to_array
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from cnnmodel.smallervggnet import SmallerVGGNet
-import matplotlib.pyplot as plt
-from imutils import paths
-import numpy as np
-import argparse
-import random
-import pickle
-import cv2
-import os
-import seaborn as sns
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from tensorflow.keras import layers
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset (i.e., directory of images)")
-ap.add_argument("-m", "--model", required=True,
-	help="path to output model")
-ap.add_argument("-l", "--labelbin", required=True,
-	help="path to output label binarizer")
-ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output accuracy/loss plot")
-args = vars(ap.parse_args())
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
-# initialize the number of epochs to train for, initial learning rate,
-#batch size, and image dimensions
-EPOCHS = 900
-INIT_LR = 1e-3
-BS = 30
-IMAGE_DIMS = (90, 120, 3)
+# Filter the data to include only car images (label 1)
+car_indices = y_train.flatten() == 1
+x_train = x_train[car_indices]
+y_train = y_train[car_indices]
 
-# initialize the data and labels
-data = []
-labels = []
+# Preprocess the data
+x_train = x_train.astype("float32") / 255.0
+y_train = keras.utils.to_categorical(y_train, num_classes=2)
 
-# grab the image paths and randomly shuffle them
-print("[INFO] loading images...")
-imagePaths = sorted(list(paths.list_images(args["dataset"])))
-random.seed(42)
-random.shuffle(imagePaths)
+# Build the CNN model
+model = keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(2, activation="softmax")
+])
 
-print("[INFO] imagePaths", imagePaths)
-# loop over the input images
-for imagePath in imagePaths:
-	# load the image, pre-process it, and store it in the data list
-	image = cv2.imread(imagePath)
-	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
-	image = img_to_array(image)
-	data.append(image)
- 
-	label = imagePath.split(os.path.sep)[-2]
-	labels.append(label)
+# Compile the model
+model.compile(optimizer="adam",
+              loss="categorical_crossentropy",
+              metrics=["accuracy"])
 
-# scale the raw pixel intensities to the range [0, 1]
-data = np.array(data, dtype="float") / 255.0
-labels = np.array(labels)
-print("[INFO] data matrix: {:.2f}MB".format(
-	data.nbytes / (1024 * 1000.0)))
+# Train the model
+model.fit(x_train, y_train, batch_size=64, epochs=10, validation_split=0.2)
 
-# binarize the labels
-lb = LabelBinarizer()
-labels = lb.fit_transform(labels)
-print("Labels", labels)
+# Evaluate the model on the test set
+x_test = x_test.astype("float32") / 255.0
+y_test = keras.utils.to_categorical(y_test, num_classes=2)
 
-(trainX, testX, trainY, testY) = train_test_split(data,
-	labels, test_size=0.15, random_state=42)
-print("trainX", trainX, trainY, testX, testY)	
-
-# construct the image generator for data augmentation
-aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
-	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-	horizontal_flip=True, fill_mode="nearest")
-	
-# initialize the model
-print("[INFO] compiling model...")
-print("Classification", len(lb.classes_))
-model = SmallerVGGNet.build(width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
-	depth=IMAGE_DIMS[2], classes=len(lb.classes_))
-opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"], run_eagerly=True)
-
-# train the model
-print("[INFO] training model...")
-H = model.fit(
-    aug.flow(trainX, trainY, batch_size=BS),
-    steps_per_epoch=len(trainX) // BS,
-    epochs=EPOCHS,
-    validation_data=(testX, testY)
-)
-
-# H = model.fit(
-#     trainX,
-#     trainY,
-#     batch_size=BS,
-#     epochs=EPOCHS,
-#     validation_data=(testX, testY)
-# )
-
-
-# Make predictions on the test set
-preds = model.predict(testX)
-
-# Convert predictions from one-hot encoding to class labels
-pred_labels = np.argmax(preds, axis=1)
-true_labels = np.argmax(testY, axis=1)
-
-# Generate confusion matrix
-cm = confusion_matrix(true_labels, pred_labels)
-print('\nAccuracy:', np.diagonal(cm).sum()/cm.sum())
-# save the model to disk
-print("[INFO] serializing network...")
-model.save(args["model"])
-
-# save the label binarizer to disk
-print("[INFO] serializing label binarizer...")
-f = open(args["labelbin"], "wb")
-f.write(pickle.dumps(lb))
-f.close()
-
-# plot the training loss and accuracy
-plt.style.use("ggplot")
-plt.figure()
-N = EPOCHS
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="upper left")
-plt.savefig(args["plot"])
-plt.show()
-
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", cbar=False)
-plt.xlabel("Predicted Labels")
-plt.ylabel("True Labels")
-plt.title("Confusion Matrix")
-plt.savefig('confusion_matrix.png')
-plt.show()
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Test loss:", test_loss)
+print("Test accuracy:", test_acc)
