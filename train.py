6d2d2ab338db6b703a746a8b3cd5529d6532c2e5
NhiHuynh <nhi@scaleflex.com>
2023-06-17 17:24:09 +0700

    add class, max pooling

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import AdamW
 
 # Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -16,31 +17,48 @@ x_train, x_val, y_train, y_val = train_test_split(
     x_train, y_train, test_size=0.2, random_state=42)
 
 # Preprocess the data
-x_train = x_train.astype("float32") / 255.0
-x_val = x_val.astype("float32") / 255.0
-x_test = x_test.astype("float32") / 255.0
+x_train = tf.image.resize(x_train, (224, 224))
+x_val = tf.image.resize(x_val, (224, 224))
+x_test = tf.image.resize(x_test, (224, 224))
+x_train = x_train / 255.0
+x_val = x_val / 255.0
+x_test = x_test / 255.0
 
-# Build the CNN model with increased network size
+# Build the CNN model with increased network size and adjusted pooling
 model = keras.Sequential([
-    layers.Conv2D(64, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+    layers.Conv2D(64, (3, 3), activation="relu", input_shape=(224, 224, 3)),
+    layers.Conv2D(64, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation="relu"),
     layers.Conv2D(128, (3, 3), activation="relu"),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(256, (3, 3), activation="relu"),
+    layers.Conv2D(256, (3, 3), activation="relu"),
+    layers.Conv2D(256, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.Conv2D(512, (3, 3), activation="relu"),
+    layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
-    layers.Dense(256, activation="relu"),
-    layers.Dense(128, activation="relu"),
+    layers.Dense(4096, activation="relu"),
+    layers.Dense(4096, activation="relu"),
     layers.Dense(10, activation="softmax")
 ])
 
-# Compile the model
-model.compile(optimizer="adam",
+# Compile the model with AdamW optimizer
+optimizer = AdamW(learning_rate=0.001, weight_decay=0.0001)
+model.compile(optimizer=optimizer,
               loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
 
 # Train the model
 model.fit(x_train, y_train, batch_size=64,
-          epochs=20, validation_data=(x_val, y_val))
+          epochs=10, validation_data=(x_val, y_val))
 
 # Evaluate the model on the test set
 test_loss, test_acc = model.evaluate(x_test, y_test)
