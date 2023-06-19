2dda1211ccf6b6278bd0a0c7fa437db8a6ef34e7
NhiHuynh <nhi@scaleflex.com>
2023-06-19 22:09:02 +0700

    add train.py

--- train.py
+++ train.py
@@ -3,6 +3,12 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 from adamw import AdamW
+from cnnmodel.smallervggnet import SmallerVGGNet
+
+INIT_LR = 1e-4
+BS = 30
+EPOCHS = 400
+IMAGE_DIMS = (224, 224, 3)
 
 # Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -25,40 +31,18 @@ x_val = x_val / 255.0
 x_test = x_test / 255.0
 
 # Build the CNN model with increased network size and adjusted pooling
-model = keras.Sequential([
-    layers.Conv2D(64, (3, 3), activation="relu", input_shape=(224, 224, 3)),
-    layers.Conv2D(64, (3, 3), activation="relu"),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, (3, 3), activation="relu"),
-    layers.Conv2D(128, (3, 3), activation="relu"),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(256, (3, 3), activation="relu"),
-    layers.Conv2D(256, (3, 3), activation="relu"),
-    layers.Conv2D(256, (3, 3), activation="relu"),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.Conv2D(512, (3, 3), activation="relu"),
-    layers.MaxPooling2D((2, 2)),
-    layers.Flatten(),
-    layers.Dense(4096, activation="relu"),
-    layers.Dense(4096, activation="relu"),
-    layers.Dense(10, activation="softmax")
-])
+model = SmallerVGGNet.build(width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
+                            depth=IMAGE_DIMS[2], classes=10)
 
 # Compile the model with AdamW optimizer
-optimizer = AdamW(learning_rate=0.001, weight_decay=0.0001)
+optimizer = AdamW(learning_rate=INIT_LR, weight_decay=0.0001)
 model.compile(optimizer=optimizer,
-              loss="sparse_categorical_crossentropy",
+              loss="categorical_crossentropy",
               metrics=["accuracy"])
 
 # Train the model
-model.fit(x_train, y_train, batch_size=64,
-          epochs=250, validation_data=(x_val, y_val))
+model.fit(x_train, y_train, batch_size=BS,
+          epochs=EPOCHS, validation_data=(x_val, y_val))
 
 # Evaluate the model on the test set
 test_loss, test_acc = model.evaluate(x_test, y_test)
@@ -66,4 +50,4 @@ print("Test loss:", test_loss)
 print("Test accuracy:", test_acc)
 
 
-# scp -P 10023 /Users/baby/Desktop/vehicle-brand-classification/train.py nhihtt@103.130.211.150:/home/nhihtt/data/vehicle
+# scp -P 10023 /Users/baby/Desktop/vehicle-brand-classification/train.py nhihtt@103.130.211.150:/home/nhihtt/data/vehicle-brand-classification
