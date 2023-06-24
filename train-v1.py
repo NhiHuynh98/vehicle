5b83a6cb51b083b742d6a66d3ce5119b741aba33
NhiHuynh <nhi@scaleflex.com>
2023-06-24 16:35:43 +0700

    train test

--- train-v1.py
+++ train-v1.py
@@ -7,8 +7,18 @@ from IPython.display import HTML
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import argparse
+import numpy as np
 
 IMAGE_SIZE = 128
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i])
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", required=True,
@@ -97,3 +107,16 @@ plt.title('Training and Validation Loss')
 plt.savefig('loss.png')
 plt.show()
 
+plt.figure(figsize=(15, 15))
+for images, labels in test_generator:
+    for i in range(6):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i])
+
+        predicted_class, confidence = predict(model, images[i])
+        actual_class = class_names[int(labels[i])]
+
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        plt.savefig(f'{actual_class}.png')
+        plt.axis("off")
+    break
