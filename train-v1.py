247dfc6702479aa8b62623b14e8f7852e37f4a42
NhiHuynh <nhi@scaleflex.com>
2023-06-24 16:25:33 +0700

    train

--- train-v1.py
+++ train-v1.py
@@ -5,7 +5,15 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from IPython.display import HTML
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import argparse
+
 IMAGE_SIZE = 128
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", required=True,
+                help="path to output model")
+args = vars(ap.parse_args())
 
 train_datagen = ImageDataGenerator(
     rescale=1./255,
@@ -26,3 +34,66 @@ for image_batch, label_batch in train_generator:
 
 class_names = list(train_generator.class_indices.keys())
 print(class_names)
+test_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=10,
+        horizontal_flip=True)
+
+test_generator = test_datagen.flow_from_directory(
+        'Cars Dataset/test',
+        target_size=(IMAGE_SIZE,IMAGE_SIZE),
+        class_mode="sparse"
+)
+for image_batch, label_batch in test_generator:
+    print(image_batch[0])
+    break
+sz = 128
+model = Sequential()
+
+# First convolution layer and pooling
+model.add(Convolution2D(32, (3, 3), input_shape=(sz, sz, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Second convolution layer and pooling
+model.add(Convolution2D(32, (3, 3), activation='relu'))
+# input_shape is going to be the pooled feature maps from the previous convolution layer
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flattening the layers
+model.add(Flatten())
+EPOCHS = 2
+# Adding a fully connected layer
+model.add(Dense(units=96, activation='relu'))
+model.add(Dropout(0.40))
+model.add(Dense(units=32, activation='relu'))
+model.add(Dense(units=7, activation='softmax')) # softmax for more than 2
+model.summary()
+model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),metrics=['accuracy'])
+history = model.fit(
+    train_generator,
+    validation_data=test_generator,
+    epochs=EPOCHS
+)
+model.save(args["model"])
+scores = model.evaluate(test_generator)
+print(scores)
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(range(EPOCHS), acc, label='Training Accuracy')
+plt.plot(range(EPOCHS), val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+plt.savefig('accuracy.png')
+
+plt.subplot(1, 2, 2)
+plt.plot(range(EPOCHS), loss, label='Training Loss')
+plt.plot(range(EPOCHS), val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.savefig('loss.png')
+plt.show()
+
