a49d4621ab46fef55c7ca1405cbe342d298b1b6c
NhiHuynh <nhi@scaleflex.com>
2023-07-07 23:38:04 +0700

    done

--- train-vgg.py
+++ train-vgg.py
@@ -1,13 +1,65 @@
 from tensorflow.keras.layers import Input,Dense,Flatten,Lambda
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
 from tensorflow.keras.models import Sequential
+from sklearn.metrics import accuracy_score
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from livelossplot.inputs.keras import PlotLossesCallback
+import os
+from keras.optimizers import Adam
+
 import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
+
+def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
+    """
+    Compiles a model integrated with VGG16 pretrained layers
+    
+    input_shape: tuple - the shape of input images (width, height, channels)
+    n_classes: int - number of classes for the output layer
+    optimizer: string - instantiated optimizer to use for training. Defaults to 'RMSProp'
+    fine_tune: int - The number of pre-trained layers to unfreeze.
+                If set to 0, all pretrained layers will freeze during training
+    """
+    
+    # Pretrained convolutional layers are loaded using the Imagenet weights.
+    # Include_top is set to False, in order to exclude the model's fully-connected layers.
+    conv_base = VGG16(include_top=False,
+                     weights='imagenet', 
+                     input_shape=input_shape)
+    
+    # Defines how many layers to freeze during training.
+    # Layers in the convolutional base are switched from trainable to non-trainable
+    # depending on the size of the fine-tuning parameter.
+    if fine_tune > 0:
+        for layer in conv_base.layers[:-fine_tune]:
+            layer.trainable = False
+    else:
+        for layer in conv_base.layers:
+            layer.trainable = False
+
+    # Create a new 'top' of the model (i.e. fully-connected layers).
+    # This is 'bootstrapping' a new top_model onto the pretrained layers.
+    top_model = conv_base.output
+    top_model = Flatten(name="flatten")(top_model)
+    top_model = Dense(4096, activation='relu')(top_model)
+    top_model = Dense(1072, activation='relu')(top_model)
+    top_model = Dropout(0.2)(top_model)
+    output_layer = Dense(n_classes, activation='softmax')(top_model)
+    
+    # Group the convolutional base and new fully-connected layers into a Model object.
+    model = Model(inputs=conv_base.input, outputs=output_layer)
+
+    # Compiles the model for training.
+    model.compile(optimizer=optimizer, 
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    return model
 
 IMAGE_SIZE = [224,224]
 
@@ -32,31 +84,93 @@ model = Model(inputs=vgg.input,outputs=prediction)    # create model object
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range = 0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+BATCH_SIZE = 32
+epochs = 5
 
-test_datagen=ImageDataGenerator(rescale=1./255)
+train_generator = ImageDataGenerator(rotation_range=90, 
+                                     brightness_range=[0.1, 0.7],
+                                     width_shift_range=0.5, 
+                                     height_shift_range=0.5,
+                                     horizontal_flip=True, 
+                                     vertical_flip=True,
+                                     validation_split=0.15,
+                                     preprocessing_function=preprocess_input) # VGG16 preprocessing
 
-bz = 32
-epochs = 20
+test_generator = ImageDataGenerator(preprocessing_function=preprocess_input) 
 
-training_set=train_datagen.flow_from_directory('Cars Dataset/train',target_size=(224,224),batch_size=bz,class_mode='categorical')
+folders=glob('Cars Dataset/Train/*')
+
+# class_subset = sorted(os.listdir(download_dir/'food-101/images'))[:10] # Using only the first 10 classes
+
+traingen = train_generator.flow_from_directory(train_path,
+		target_size=(224, 224),
+		class_mode='categorical',
+		classes=folders,
+		subset='training',
+		batch_size=BATCH_SIZE, 
+		shuffle=True,
+		seed=42)
+
+validgen = train_generator.flow_from_directory(train_path,
+		target_size=(224, 224),
+		class_mode='categorical',
+		classes=folders,
+		subset='validation',
+		batch_size=BATCH_SIZE,
+		shuffle=True,
+		seed=42)
+
+testgen = test_generator.flow_from_directory(test_path,
+		target_size=(224, 224),
+		class_mode=None,
+		classes=folders,
+		batch_size=1,
+		shuffle=False,
+		seed=42)
+
+input_shape = (224, 224, 3)
+optim_1 = Adam(learning_rate=0.001)
+n_classes=10
+
+n_steps = traingen.samples // BATCH_SIZE
+n_val_steps = validgen.samples // BATCH_SIZE
+n_epochs = 50
+
+# First we'll train the model without Fine-tuning
+vgg_model = create_model(input_shape, n_classes, optim_1, fine_tune=0)
+
+plot_loss_1 = PlotLossesCallback()
+
+# ModelCheckpoint callback - save best weights
+tl_checkpoint_1 = ModelCheckpoint(filepath='tl_model_v1.weights.best.hdf5',
+                                  save_best_only=True,
+                                  verbose=1)
+
+# EarlyStopping
+early_stop = EarlyStopping(monitor='val_loss',
+                           patience=10,
+                           restore_best_weights=True,
+                           mode='min')
 
 
-test_set=test_datagen.flow_from_directory('Cars Dataset/test',target_size=(224,224),batch_size=32,class_mode='categorical')
+vgg_history = model.fit(traingen,
+	batch_size=BATCH_SIZE,
+	epochs=n_epochs,
+	validation_data=validgen,
+	steps_per_epoch=n_steps,
+	validation_steps=n_val_steps,
+	callbacks=[tl_checkpoint_1, early_stop, plot_loss_1],
+	verbose=1)
 
-r=model.fit(
-    training_set,
-    validation_data=test_set,
-    epochs=epochs,
-    steps_per_epoch=len(training_set),
-    validation_steps=len(test_set)
-)
+# Generate predictions
+vgg_model.load_weights('tl_model_v1.weights.best.hdf5') # initialize the best trained weights
 
-y_pred=model.predict(test_set)
+true_classes = testgen.classes
+class_indices = traingen.class_indices
+class_indices = dict((v,k) for k,v in class_indices.items())
 
-print("TEST", y_pred)
+vgg_preds = vgg_model.predict(testgen)
+vgg_pred_classes = np.argmax(vgg_preds, axis=1)
+
+vgg_acc = accuracy_score(true_classes, vgg_pred_classes)
+print("VGG16 Model Accuracy without Fine-Tuning: {:.2f}%".format(vgg_acc * 100))
