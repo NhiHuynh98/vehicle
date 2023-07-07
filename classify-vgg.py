13ed2369a74d44d3b22284148697f540a652f281
NhiHuynh <nhi@scaleflex.com>
2023-07-08 00:40:10 +0700

    fs

--- classify-vgg.py
+++ classify-vgg.py
@@ -1,6 +1,5 @@
 
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+import numpy as np
 from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
@@ -20,7 +19,7 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB format
 
 image = cv2.resize(image, (224, 224))
 # convert the image pixels to a numpy array
-image = img_to_array(image)
+image = np.array(image)
 # reshape data for the model
 image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 # prepare the image for the VGG model
