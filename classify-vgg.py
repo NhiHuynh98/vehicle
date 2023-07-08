47980d14b75d74bcefe87de9ea35643ac48111a0
NhiHuynh <nhi@scaleflex.com>
2023-07-08 08:07:54 +0700

    test

--- classify-vgg.py
+++ classify-vgg.py
@@ -6,31 +6,39 @@ from keras.applications.vgg16 import VGG16
 import argparse
 import cv2
 from keras.models import load_model
+from keras.preprocessing import image
 
 # load the model
-model = load_model('tl_model_v1.weights.best.hdf5')
+model = load_model('model.h5')
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--image", required=True,
                 help="input image need to classify")
 args = vars(ap.parse_args())
-# load an image from file
-image = cv2.imread(args['image'])
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB format
 
-image = cv2.resize(image, (224, 224))
-# convert the image pixels to a numpy array
-image = np.array(image)
-# reshape data for the model
-image = np.expand_dims(image, axis=0)
-#image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-# prepare the image for the VGG model
-image = preprocess_input(image)
-# predict the probability across all output classes
-yhat = model.predict(image)
+# load an image from file
+# image = cv2.imread(args["image"])
+
+# image = img_to_array(image)
+# image = np.expand_dims(image, axis=0)
+
+#Load flower jpeg image from local and set target size to 224 x 224
+img = image.load_img(args["image"], target_size=(224,224))
+
+#convert image to array
+input_img = image.img_to_array(img)
+input_img = np.expand_dims(input_img, axis=0)
+
+print("[INFO] classifying image...")
+proba = model.predict(image)
+print("[PROBA] proba", proba)
+print("[PROBA] proba[0]", proba[0])
 # convert the probabilities to class labels
-label = decode_predictions(yhat)
+label = decode_predictions(proba, top=5)
+print("[LABEL]", label)
+
 # retrieve the most likely result, e.g. highest probability
 label = label[0][0]
+print("[LABEL]1", label)
 # print the classification
 print('%s (%.2f%%)' % (label[1], label[2]*100))
