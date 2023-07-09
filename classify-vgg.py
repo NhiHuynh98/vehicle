bd49932115f852cd53660b26a8c51d773ce5066b
NhiHuynh <nhi@scaleflex.com>
2023-07-09 19:29:19 +0700

    test

--- classify-vgg.py
+++ classify-vgg.py
@@ -29,16 +29,27 @@ img = image.load_img(args["image"], target_size=(224,224))
 input_img = image.img_to_array(img)
 input_img = np.expand_dims(input_img, axis=0)
 
-print("[INFO] classifying image...")
-proba = model.predict(image)
-print("[PROBA] proba", proba)
-print("[PROBA] proba[0]", proba[0])
-# convert the probabilities to class labels
-label = decode_predictions(proba, top=5)
-print("[LABEL]", label)
+reshaped_prediction = np.zeros((1, 1000))
+reshaped_prediction[0, :7] = input_img
 
-# retrieve the most likely result, e.g. highest probability
-label = label[0][0]
-print("[LABEL]1", label)
-# print the classification
-print('%s (%.2f%%)' % (label[1], label[2]*100))
+# Decode the predictions
+decoded_predictions = decode_predictions(reshaped_prediction, top=5)
+
+# Print the top predicted classes
+print("Top predicted classes:")
+for _, class_name, class_prob in decoded_predictions[0]:
+    print(class_name, ":", class_prob)
+    
+# print("[INFO] classifying image...")
+# proba = model.predict(input_img)
+# print("[PROBA] proba", proba)
+# print("[PROBA] proba[0]", proba[0])
+# # convert the probabilities to class labels
+# label = decode_predictions(proba, top=5)
+# print("[LABEL]", label)
+
+# # retrieve the most likely result, e.g. highest probability
+# label = label[0][0]
+# print("[LABEL]1", label)
+# # print the classification
+# print('%s (%.2f%%)' % (label[1], label[2]*100))
